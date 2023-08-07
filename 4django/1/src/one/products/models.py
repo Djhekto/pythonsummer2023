db79115fb88e98306d61b1dc7c943293ab11db00
DjHekto <khametovmv@my.msu.ru>
2023-08-07 11:26:03 +0300

    secondModel

--- 4django/1/src/one/products/models.py
+++ 4django/1/src/one/products/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Product(models.Model):
-    title       = models.TextField()
-    description = models.TextField()
-    price       = models.TextField()
+    title       = models.CharField(max_length=30)
+    description = models.TextField(blank=True,null=True)
+    price       = models.DecimalField(decimal_places=2,max_digits=10)
     extra       = models.TextField(default="spam")
     
     
